fa60f7c0c02bb4825205cece2eae63bedfd1fc9c
mwhite14 <mwhite14@ksu.edu>
2014-02-15 17:53:55 -0600

    Added NDK stuff

--- CodeDayKCRunningApp/src/com/example/codedaykcrunningapp/MainActivity.java
+++ CodeDayKCRunningApp/src/com/example/codedaykcrunningapp/MainActivity.java
@@ -3,13 +3,33 @@ package com.example.codedaykcrunningapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	static
+	{
+		System.loadLibrary("ndk");		
+	}
+	
+	// References the method in C file
+	private native String helloLog(String logThis);
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	TextView t = (TextView)findViewById(R.id.textView1);
+            	t.setText(helloLog("Hey")); 
+            }
+        });
 	}
 
 	@Override
